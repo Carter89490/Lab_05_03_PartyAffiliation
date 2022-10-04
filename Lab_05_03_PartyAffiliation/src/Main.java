0f677536d8e1cf114271f9f5eca47c0e7b68536f
Carter Holsinger <113468540+Carter89490@users.noreply.github.com>
2022-10-04 14:29:14 -0400

    Add files via upload

---
+++ Lab_05_03_PartyAffiliation/src/Main.java
@@ -0,0 +1,18 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String party = "O";
+        if (party == "R" ) {
+            System.out.println("Your an elephant");
+        }
+        else if (party == "D") {
+                System.out.println("Your a donkey");
+            }
+        else if (party == "I"){
+            System.out.println("Your an indenpent person");
+        }
+        else
+            System.out.println("your with another party");
+    }
+}
